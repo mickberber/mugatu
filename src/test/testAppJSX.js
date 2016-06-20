3455a3a7b9e2f8afdf82326ddcf39e30476602a8
Michael Berber <mickberber@icloud.com>
2016-06-20 09:21:24 -0700

    test chooseType

--- src/test/testAppJSX.js
+++ src/test/testAppJSX.js
@@ -77,5 +77,59 @@ module.exports = write.testFile(
         tests.compareYield(
             (() => { removePiano(); return state.pianos.length; })(), 0
         )
+    ),
+    write.description('Choose Type should change state.type to MUGATUS_SILLY_MODE when passed MUGATUS_SILLY_MODE')(
+        tests.compareYield(
+            (() => { chooseType('MUGATUS_SILLY_MODE', './../assets/mugatu-o.gif'); return state.type; })(), 
+            'MUGATUS_SILLY_MODE'
+        )
+    ),
+    write.description('Choose Type should change state.img to ./../assets/mugatu-o.gif when passed MUGATUS_SILLY_MODE')(
+        tests.compareYield(
+            (() => { return state.img; })(), 
+            './../assets/mugatu-o.gif'
+        )
+    ),
+    write.description('Choose Type should change state.audio to audio.sillyAudio when passed MUGATUS_SILLY_MODE')(
+        tests.compareArrays(
+            (() => { return state.audio; })(), 
+            audio.sillyAudio
+        )
+    ),
+    write.description('Choose Type should change state.type to PIANO when passed PIANO')(
+        tests.compareYield(
+            (() => { chooseType('PIANO', './../assets/piano.jpg'); return state.type; })(), 
+            'PIANO'
+        )
+    ),
+    write.description('Choose Type should change state.img to ./../assets/piano.jpg when passed PIANO')(
+        tests.compareYield(
+            (() => { return state.img; })(), 
+            './../assets/piano.jpg'
+        )
+    ),
+    write.description('Choose Type should change state.audio to audio.pianoAudio when passed PIANO')(
+        tests.compareArrays(
+            (() => { return state.audio; })(), 
+            audio.pianoAudio
+        )
+    ),
+    write.description('Choose Type should change state.type to NECKTIE when passed NECKTIE')(
+        tests.compareYield(
+            (() => { chooseType('NECKTIE', './../assets/pknecktie5.png'); return state.type; })(), 
+            'NECKTIE'
+        )
+    ),
+    write.description('Choose Type should change state.img to ./../assets/pknecktie5.png when passed NECKTIE')(
+        tests.compareYield(
+            (() => { return state.img; })(), 
+            './../assets/pknecktie5.png'
+        )
+    ),
+    write.description('Choose Type should change state.audio to audio.necktieAudio when passed NECKTIE')(
+        tests.compareArrays(
+            (() => { return state.audio; })(), 
+            audio.necktieAudio
+        )
     )
 );
