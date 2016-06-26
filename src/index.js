c614a2682b88849b8dd9ccd6eaded54f8f5e9988
Michael Berber <mickberber@icloud.com>
2016-06-25 20:32:21 -0700

    reintroduce redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { Router, Route, hashHistory } from 'react-router';
 
 import App from './components/app';
 import Splash from './components/splash';
 
+import Reducers from './reducers';
+let store = createStore(Reducers);
+
 ReactDOM.render(
+  <Provider store={store}>
     <Router history={hashHistory}>
       <Route path='/' component={Splash}/>
       <Route path='/app' component={App}/>
     </Router>
+  </Provider>
   , document.querySelector('.attach'));
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import Pianos from './pianos';
+
+const Reducers = combineReducers({
+    Pianos
+});
+
+export default Reducers;
